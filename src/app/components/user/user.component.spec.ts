d112179cd4af60b9aa0274eec533bd64aaeee221
WeWake <vvakekumar@gmail.com>
2022-06-16 00:41:30 +0530

    Checking router link value in test

--- src/app/components/user/user.component.spec.ts
+++ src/app/components/user/user.component.spec.ts
@@ -1,7 +1,20 @@
+import { Directive, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-
 import { UserComponent } from './user.component';
+
+@Directive({
+  selector: '[routerLink]',
+  host: { '(click)': 'onClick()' },
+})
+export class RouterLinkDirectiveStub {
+  @Input('routerLink') linkParams: any;
+  navigatedTo: any = null;
+
+  onClick() {
+    this.navigatedTo = this.linkParams;
+  }
+}
 
 describe('UserComponent', () => {
   let component: UserComponent;
@@ -9,7 +22,7 @@ describe('UserComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [UserComponent],
+      declarations: [UserComponent, RouterLinkDirectiveStub],
     }).compileComponents();
   });
 
@@ -39,5 +52,16 @@ describe('UserComponent', () => {
     expect(
       fixture.debugElement.query(By.css('h4')).nativeElement.textContent
     ).toContain('Vivek');
+  });
+
+  it('should have a link to the user details', () => {
+    component.user = { id: 100, name: 'Vivek' };
+    fixture.detectChanges();
+    const routerLink = fixture.debugElement
+      .query(By.directive(RouterLinkDirectiveStub))
+      .injector.get(RouterLinkDirectiveStub);
+
+    expect(routerLink).toBeTruthy();
+    expect(routerLink.linkParams).toEqual(['/users', 100]);
   });
 });
